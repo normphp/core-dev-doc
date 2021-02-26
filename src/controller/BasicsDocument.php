56cf0bc563cd8c653176c142515ba82274016bcf
pi <pizepei@pizepei.com>
2021-02-26 21:20:45 +0800

    拆分开发者文档功能为独立包normphp-core/dev-doc

--- src/controller/BasicsDocument.php
+++ src/controller/BasicsDocument.php
@@ -1,35 +1,36 @@
 <?php
 /**
  * Class Deploy
- * @title 閮ㄧ讲鍩虹绫�
+ * @title 部署基础类
  */
 namespace normphpCore\devDoc\controller;
+
 use authority\app\Resource;
 use normphp\staging\Controller;
 use normphp\staging\Request;
-use normphp\staging\service\DocumentService;
+use normphpCore\devDoc\service\DocumentService;
 use ZipArchive;
 
 class BasicsDocument extends Controller
 {
     /**
-     * 鍩虹鎺у埗鍣ㄤ俊鎭�
+     * 基础控制器信息
      */
     const CONTROLLER_INFO = [
         'User'=>'pizepei',
-        'title'=>'鏂囨。鎺у埗鍣�',//鎺у埗鍣ㄦ爣棰�
-        'namespace'=>'',//闂ㄩ潰鎺у埗鍣ㄥ懡鍚嶇┖闂�
-        'basePath'=>'/document/',//鍩虹璺敱
+        'title'=>'文档控制器',//控制器标题
+        'namespace'=>'',//门面控制器命名空间
+        'basePath'=>'/document/',//基础路由
     ];
 
     protected $path  = '';
     /**
      * @param \normphp\staging\Request $Request
-     *      path [object] 璺緞鍙傛暟
-     *          type [string] 璺緞
+     *      path [object] 路径参数
+     *          type [string] 路径
      * @return array [html]
-     * @title  鏂囨。鍏ュ彛锛堝紑鍙戝姪鎵嬶級
-     * @explain 鏂囨。鍏ュ彛锛圓PI鏂囨。銆佹潈闄愭枃妗ｃ�佸叕鍏辫祫婧愭枃妗ｏ級
+     * @title  文档入口（开发助手）
+     * @explain 文档入口（API文档、权限文档、公共资源文档）
      * @router get index/:type[string].html debug:true
      * @throws \Exception
      */
@@ -42,65 +43,64 @@ class BasicsDocument extends Controller
             'layui.js'=>'https://www.layuicdn.com/layui-v2.5.5/layui.js',
             'local.layui.css'=>\Deploy::VIEW_RESOURCE_PREFIX.'/start/layui/css/layui.css',
             'local.layui.js'=>\Deploy::VIEW_RESOURCE_PREFIX.'/start/layui/layui.js',
-            'VIEW_RESOURCE_PREFIX'=>\Deploy::VIEW_RESOURCE_PREFIX,
             'MODULE_PREFIX'=>\Deploy::MODULE_PREFIX===''?'':'/'.\Deploy::MODULE_PREFIX,
             'jsonDataName'=>$this->app->__INIT__['ReturnJsonData'],
         ];
-        $path = dirname(__DIR__).DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'document'.DIRECTORY_SEPARATOR;
+        $path = dirname(__DIR__).DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'Document'.DIRECTORY_SEPARATOR;
         return $this->view($name,$data,$path,'html',false);
     }
     /**
      * @param \normphp\staging\Request $Request html
-     *      path [object] 璺緞鍙傛暟
-     *          name [string required] 鎵╁睍鏂囦欢鍚嶇О
+     *      path [object] 路径参数
+     *          name [string required] 扩展文件名称
      * @return array [js]
-     * @title  鏂囨。layui Extends
-     * @explain  鏂囨。layui Extends
+     * @title  文档layui Extends
+     * @explain  文档layui Extends
      * @router get layui/extends/:name[string].js debug:true
      * @throws \Exception
      */
     public function layuiExtends(Request $Request)
     {
-        $path = dirname(__DIR__).DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'document'.DIRECTORY_SEPARATOR;
+        $path = dirname(__DIR__).DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'Document'.DIRECTORY_SEPARATOR;
         return $this->view($Request->path('name'),[],$path,'js',false);
 
     }
 
     /**
      * @return array [json]
-     *      data [raw] 鑿滃崟鏁版嵁
-     * @title  API鏂囨。 渚ц竟瀵艰埅
-     * @explain  渚ц竟瀵艰埅
+     *      data [raw] 菜单数据
+     * @title  API文档 侧边导航
+     * @explain  侧边导航
      * @router get nav-list debug:true
      * @throws \Exception
      */
     public function navList()
     {
-        return $this->succeed($this->app->Route()->noteBlock);
+        return $this->succeed($this->app->Route()->noteBlock,'',200);
     }
     /**
      * @Author pizepei
      * @Created 2019/2/12 23:01
      *
      * @param \normphp\staging\Request $Request
-     *      get [object] 璺緞鍙傛暟
-     *          father [string] 鐖惰矾寰�
-     *          index [string] 褰撳墠璺緞
+     *      get [object] 路径参数
+     *          father [string] 父路径
+     *          index [string] 当前路径
      * @return array [json]
-     *      data [object] 鎺у埗鍣ㄦ暟鎹�
-     *          fatherInfo [object] 鎺у埗鍣ㄦ暟鎹�
-     *              baseAuthGroup [raw] 鍩虹鏉冮檺
-     *              route [raw] 璺敱璇︾粏
-     *              index [string] 鍛藉悕绌洪棿
-     *              title [string] 鎺у埗鍣ㄦ爣棰�
-     *              class [string] 鎺у埗鍣ㄥ悕
-     *              User [string] 鍒涘缓鑰�
-     *              basePath [string] 鎺у埗鍣ㄦ牴璺敱
-     *              authGroup [raw] 鎺у埗鍣ㄦ潈闄愬垎缁�
-     *              baseAuth [string] 鎺у埗鍣ㄦ牴鏉冮檺
-     *          info [raw] 璇︾粏鏁版嵁
-     * @title  鎺у埗鍣ㄦ枃妗ｄ俊鎭�
-     * @explain  鏍规嵁鐐瑰嚮渚ц竟瀵艰埅鑾峰彇瀵瑰簲鐨勮幏鍙朅PI鏂囨。淇℃伅
+     *      data [object] 控制器数据
+     *          fatherInfo [object] 控制器数据
+     *              baseAuthGroup [raw] baseAuthGroup
+     *              index [string] 命名空间
+     *              title [string] 控制器标题
+     *              class [string] 控制器名
+     *              User [string] 创建者
+     *              route [raw] route
+     *              basePath [string] 控制器根路由
+     *              authGroup [raw] 控制器权限分组
+     *              baseAuth [string] 控制器根权限
+     *          info [raw] 详细数据
+     * @title  控制器文档信息
+     * @explain  根据点击侧边导航获取对应的获取API文档信息
      * @router get index-nav debug:true
      * @throws \Exception
      */
@@ -115,9 +115,8 @@ class BasicsDocument extends Controller
         }
         return $this->succeed([
                 'fatherInfo'=>$fatherInfo,
-                'info'=>$info
-            ]
-        );
+                'info'=>$info]
+        ,'',200);
 
     }
 
@@ -126,17 +125,18 @@ class BasicsDocument extends Controller
      * @Created 2019/2/14 23:01
      *
      * @param \normphp\staging\Request $Request
-     *      get [object] get鍙傛暟
-     *          father [string] 鐖惰矾寰�
-     *          index [string required] 褰撳墠璺緞
-     *          type [string required] 鍙傛暟绫诲瀷
+     *      get [object] get参数
+     *          father [string] 父路径
+     *          index [string required] 当前路径
+     *          type [string required] 参数类型
      * @return array [json]
-     *      data [objectList] 鏁版嵁e
-     *              field [string] 鍙傛暟鍚嶅瓧
-     *              type [string] 鍙傛暟鏁版嵁绫诲瀷
-     *              explain [string] 鍙傛暟璇存槑
-     * @title  鑾峰彇API鐨勮姹傚弬鏁颁俊鎭�
-     * @explain  鏍规嵁鐐瑰嚮渚ц竟瀵艰埅鑾峰彇瀵瑰簲鐨勮幏鍙朅PI鏂囨。淇℃伅
+     *      data [objectList] 数据
+     *              restrain [string] 限制约束
+     *              field [string] 参数名字
+     *              type [string] 参数数据类型
+     *              explain [string] 参数说明
+     * @title  获取API的请求参数信息
+     * @explain  根据点击侧边导航获取对应的获取API文档信息
      * @router get request-param
      * @throws \Exception
      */
@@ -156,26 +156,25 @@ class BasicsDocument extends Controller
         }else{
             $infoData = [];
         }
-        return $this->succeed($infoData??[],'鑾峰彇'.$input['index'].'鎴愬姛',0);
+        return $this->succeed($infoData??[],'获取'.$input['index'].'成功',200);
     }
-
     /**
      * @Author pizepei
      * @Created 2019/2/14 23:01
      *
      * @param \normphp\staging\Request $Request
-     *      get [object] get鍙傛暟
-     *          father [string required] 鐖惰矾寰�
-     *          index [string required] 褰撳墠璺緞
-     *          type [string required] 鍙傛暟绫诲瀷
+     *      get [object] get参数
+     *          father [string required] 父路径
+     *          index [string required] 当前路径
+     *          type [string required] 参数类型
      * @return array [json]
-     *      data [objectList] 鏁版嵁
-     *          field [string] 鍙傛暟鍚嶅瓧
-     *          type [string] 鍙傛暟鏁版嵁绫诲瀷
-     *          explain [string] 鍙傛暟璇存槑
-     *          restrain [string] 鍙傛暟绾︽潫
-     * @title  鑾峰彇API鐨勮繑鍥炲弬鏁颁俊鎭�
-     * @explain  鏍规嵁鐐瑰嚮渚ц竟瀵艰埅鑾峰彇瀵瑰簲鐨勮幏鍙朅PI鏂囨。淇℃伅
+     *      data [objectList] 数据
+     *          field [required] 参数名字
+     *          type [string] 参数数据类型
+     *          explain [string] 参数说明
+     *          restrain [string] 参数约束
+     * @title  获取API的返回参数信息
+     * @explain  根据点击侧边导航获取对应的获取API文档信息
      * @router get return-param debug:true
      * @throws \Exception
      */
@@ -184,7 +183,7 @@ class BasicsDocument extends Controller
         $input = $Request->input();
         $info = $this->app->Route()->noteBlock[$input['father']]['route'][$input['index']]??null;
         if($info['ReturnType'] != $input['type']){
-            return $this->succeed([],'鑾峰彇1'.$input['index'].'鎴愬姛',0);
+            return $this->succeed([],'获取1'.$input['index'].'成功',200);
         }
         if(!empty($info)){
             $info['index'] = $input['index'];
@@ -198,7 +197,7 @@ class BasicsDocument extends Controller
         }else{
             $info = [];
         }
-        return $this->succeed($infoData??[],'鑾峰彇'.$input['index'].'鎴愬姛',0);
+        return $this->succeed($infoData??[],'获取'.$input['index'].'成功',200);
     }
 
 
@@ -206,13 +205,13 @@ class BasicsDocument extends Controller
      * @Author pizepei
      * @Created 2019/4/25 14:01
      * @param \normphp\staging\Request $Request
-     *      get [object] get鍙傛暟
-     *          projectId [string] 椤圭洰id
+     *      get [object] get参数
+     *          projectId [string] 项目id
      * @return array [json]
      *      data [raw]
-     *          title [string] 鏍囬
-     * @title  鑾峰彇鏉冮檺鏍�
-     * @explain  鏍规嵁鐐瑰嚮渚ц竟瀵艰埅鑾峰彇瀵瑰簲鐨勮幏鍙朅PI鏂囨。淇℃伅
+     *          title [string] 标题
+     * @title  获取权限树
+     * @explain  根据点击侧边导航获取对应的获取API文档信息
      * @router get jurisdiction-list debug:true
      * @throws \Exception
      */
@@ -220,24 +219,24 @@ class BasicsDocument extends Controller
     {
         return $this->succeed(
             \PermissionsInfo::children
-            ,'鑾峰彇鎴愬姛');
+            ,'获取成功',200);
     }
 
     /**
-     * @Author 鐨辰鍩�
+     * @Author 皮泽培
      * @Created 2019/5/18 17:57
      * @param Request $Request
-     *   path [object] 璺緞鍙傛暟
-     *   get [object] 璺緞鍙傛暟
-     *   post [object] post鍙傛暟
-     *      name [string] 濮撳悕
-     *   rule [object] 鏁版嵁娴佸弬鏁�
-     * @return array [json] 瀹氫箟杈撳嚭杩斿洖鏁版嵁
+     *   path [object] 路径参数
+     *   get [object] 路径参数
+     *   post [object] post参数
+     *      name [string] 姓名
+     *   rule [object] 数据流参数
+     * @return array [json] 定义输出返回数据
      *      id [uuid] uuid
-     *      name [object] 鍚屽鍚嶅瓧
-     * @title  IDE閫傞厤閰嶇疆鍖�
-     * @explain IDE閫傞厤閰嶇疆鍖�
-     * @authExtend UserExtend.list:鎷撳睍鏉冮檺
+     *      name [object] 同学名字
+     * @title  IDE适配配置包
+     * @explain IDE适配配置包
+     * @authExtend UserExtend.list:拓展权限
      * @baseAuth Resource:public
      * @throws \Exception
      * @router post exportPhpStormSettings
@@ -246,7 +245,7 @@ class BasicsDocument extends Controller
     {
         if($Request->post('name') === 'settings.zip' || $Request->post('name') === 'settings')
         {
-            throw new \Exception('涓嶈兘涓簊ettings鍏抽敭瀛�');
+            throw new \Exception('不能为settings关键字');
         }
         $zip = new ZipArchive();
         $path = "..".DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR.'PhpStormSettings'.DIRECTORY_SEPARATOR;
@@ -254,11 +253,11 @@ class BasicsDocument extends Controller
         $file = $path.'settings.zip';
         /**
          *
-         * 鎬庝箞涓嬭浇锛�
+         * 怎么下载？
          */
         if ($zip->open($file) === true){
 
-            $mcw = $zip->extractTo($route);//瑙ｅ帇鍒�$route杩欎釜鐩綍涓�
+            $mcw = $zip->extractTo($route);//解压到$route这个目录中
 
             $zip->close();
         }
@@ -269,8 +268,8 @@ class BasicsDocument extends Controller
      * @Created 2019/4/23 23:02
      * @param \normphp\staging\Request $Request
      * @return array [json]
-     * @title  妗嗘灦寮�鍙戞枃妗ｉ《閮ㄨ彍鍗�
-     * @explain 妗嗘灦寮�鍙戞枃妗ｉ《閮ㄨ彍鍗�
+     * @title  框架开发文档顶部菜单
+     * @explain 框架开发文档顶部菜单
      * @router get normative/new
      */
     public function messageNew(Request $Request)
@@ -280,30 +279,30 @@ class BasicsDocument extends Controller
             "HelloWorld"=> [
                 'title'=>'Hello world',
                 'route'=>[
-                    'purpose'=>['title'=>'寮�鍙戝垵琛�'],
-                    'character'=>['title'=>'妗嗘灦鐗规��'],
-                    'standard'=>['title'=>'寮�鍙戣鑼�'],
-                    'environment'=>['title'=>'寮�鍙戠幆澧�'],
-                    'saas'=>['title'=>'SAAS妯″紡'],
-                    'Docker'=>['title'=>'Docker鏀寔'],
-                    'production'=>['title'=>'鐢熶骇鐜'],
+                    'purpose'=>['title'=>'开发初衷'],
+                    'character'=>['title'=>'框架特性'],
+                    'standard'=>['title'=>'开发规范'],
+                    'environment'=>['title'=>'开发环境'],
+                    'saas'=>['title'=>'SAAS模式'],
+                    'Docker'=>['title'=>'Docker支持'],
+                    'production'=>['title'=>'生产环境'],
                 ]
             ],
             "note"=> [
-                'title'=>'娉ㄨВ璺敱',
+                'title'=>'注解路由',
                 'route'=>[
-                    'purpose'=>['title'=>'鍏ラ棬'],
-                    'character'=>['title'=>'鎺у埗鍣ㄦ敞瑙�'],
-                    'standard'=>['title'=>'鏂规硶娉ㄨВ'],
-                    'environment'=>['title'=>'鏉冮檺娉ㄨВ'],
-                    'saas'=>['title'=>'璇锋眰杩囨护'],
-                    'Docker'=>['title'=>'杈撳嚭杩囨护'],
-                    'production'=>['title'=>'鐢熶骇鐜'],
+                    'purpose'=>['title'=>'入门'],
+                    'character'=>['title'=>'控制器注解'],
+                    'standard'=>['title'=>'方法注解'],
+                    'environment'=>['title'=>'权限注解'],
+                    'saas'=>['title'=>'请求过滤'],
+                    'Docker'=>['title'=>'输出过滤'],
+                    'production'=>['title'=>'生产环境'],
                 ]
             ],
         ];
 
-        return $this->succeed($data);
+        return $this->succeed($data,'',200);
     }
 
 
@@ -313,13 +312,13 @@ class BasicsDocument extends Controller
      * @Created 2019/4/23 23:02
      * @param \normphp\staging\Request $Request
      * @return array [json]
-     * @title  妗嗘灦寮�鍙戞枃妗ｅ垵濮嬪寲鏁版嵁
-     * @explain 妗嗘灦寮�鍙戞枃妗ｅ垵濮嬪寲鏁版嵁涓昏鏄繑鍥炵姸鎬佺爜瀹氫箟
+     * @title  框架开发文档初始化数据
+     * @explain 框架开发文档初始化数据主要是返回状态码定义
      * @router get init-data
      */
     public function initData(Request $Request)
     {
-        /** 鏁版嵁鏋勯��*/
+        /** 数据构造*/
         $dataStructure =[
             'ErrorReturnJsonMsg'    => $this->app->__INIT__['ErrorReturnJsonMsg'],
             'ErrorReturnJsonCode'   => $this->app->__INIT__['ErrorReturnJsonCode'],
@@ -333,37 +332,37 @@ class BasicsDocument extends Controller
         return ['dataStructure'=>$dataStructure,'nav'=>self::_NAV_];
     }
     /**
-     * 鏂囨。涓績鑿滃崟
+     * 文档中心菜单
      */
     const _NAV_ = <<<ABC
-<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/document.html">API鏂囨。</a></li>
-<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/authority.html">鏉冮檺鏂囨。</a></li>
-<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/code.html">鐘舵�佺爜鏂囨。</a></li>
-<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/code.html">鐘舵�佺爜鏂囨。</a></li>
+<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/document.html">API文档</a></li>
+<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/authority.html">权限文档</a></li>
+<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/code.html">状态码文档</a></li>
+<li class="layui-nav-item"><a href="{{MODULE_PREFIX}}/document/index/code.html">状态码文档</a></li>
 <li class="layui-nav-item">
-    <a href="javascript:;">璧勬簮鏂囨。</a>
+    <a href="javascript:;">资源文档</a>
     <dl class="layui-nav-child">
-        <dd><a href="">妗嗘灦鏂囨。</a></dd>
-        <dd><a href="">鍥炬爣璧勬簮</a></dd>
-        <dd><a href="">鍏叡API</a></dd>
-        <dd><a href="">鎺堟潈绠＄悊</a></dd>
+        <dd><a href="">框架文档</a></dd>
+        <dd><a href="">图标资源</a></dd>
+        <dd><a href="">公共API</a></dd>
+        <dd><a href="">授权管理</a></dd>
     </dl>
 </li>
 <li class="layui-nav-item">
-    <a href="javascript:;">鎺у埗鍙�</a>
+    <a href="javascript:;">控制台</a>
     <dl class="layui-nav-child">
-        <dd><a href="">鍒濆鍖栨枃浠�</a></dd>
-        <dd><a href="">浜ゆ帴涓績</a></dd>
-        <dd><a href="">瀹夊叏涓績</a></dd>
+        <dd><a href="">初始化文件</a></dd>
+        <dd><a href="">交接中心</a></dd>
+        <dd><a href="">安全中心</a></dd>
     </dl>
 </li>
 
 <li class="layui-nav-item">
-    <a href="javascript:;">椤圭洰绠＄悊</a>
+    <a href="javascript:;">项目管理</a>
     <dl class="layui-nav-child">
-        <dd><a href="">浜哄憳绠＄悊</a></dd>
-        <dd><a href="">瑙掕壊绠＄悊</a></dd>
-        <dd><a href="">椤圭洰閰嶇疆</a></dd>
+        <dd><a href="">人员管理</a></dd>
+        <dd><a href="">角色管理</a></dd>
+        <dd><a href="">项目配置</a></dd>
     </dl>
 </li>
 ABC;
